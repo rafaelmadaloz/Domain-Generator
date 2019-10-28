878e70a5f1278f57125a4b714cc8b16347b35831
Rafael Madalóz <rafaelmadaloz@outlook.com>
2019-10-28 18:41:18 -0300

    Project developed until connection with GraphQL

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>Gerador de Domínios</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL e Node</h6>
+    </div>
+    <div id="main">
+      <DomainList></DomainList>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import DomainList from "./components/DomainList";
+
+export default {
+  name: "app",
+  components: {
+    DomainList
+  }
+};
+</script>
+
+<style>
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+</style>
